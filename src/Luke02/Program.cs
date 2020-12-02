23f5a73ec45c1644c82d3b364f229af237cc49b0
Jon Grythe Stødle <jon.stodle@webstep.no>
2020-12-02 09:18:51 +0100

    Add Luke02

---
+++ src/Luke02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Luke02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var primes = File.ReadAllText("primes.txt")
+				.Split(' ')
+				.Select(s => s.Trim())
+				.Where(s => !string.IsNullOrWhiteSpace(s))
+				.Select(int.Parse)
+				.Reverse()
+				.ToList();
+
+			var count = 0;
+			var baseNo = 0;
+			for (var i = 0; i < 5_433_000; i++)
+			{
+				if (i.ToString().Contains('7'))
+				{
+					var prime = primes.First(p => p <= i);
+					i += prime;
+				}
+				else
+				{
+					count++;
+				}
+			}
+			
+			Console.WriteLine(count);
+		}
+	}
+}
---
+++ src/fsharp/Luke02/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let primes =
+        File.ReadAllText "primes.txt"
+        |> (fun s -> s.Split(' '))
+        |> Seq.map (fun s -> s.Trim())
+        |> Seq.filter (fun s -> String.IsNullOrWhiteSpace(s) = false)
+        |> Seq.map int
+        |> Seq.toList
+
+    [ 0 .. 5_433_000 ]
+    |> Seq.fold (fun pair curr ->
+        match pair with
+        | (skip, tally) when skip > 0 -> (skip - 1, tally)
+        | (_, tally) when curr.ToString().Contains('7') -> (primes |> List.findBack (fun i -> i <= curr), tally)
+        | (_, tally) -> (0, tally + 1)) (0, 0)
+    |> (fun (_, tally) -> tally)
+    |> printfn "%i"
+    |> ignore
+
+    0
