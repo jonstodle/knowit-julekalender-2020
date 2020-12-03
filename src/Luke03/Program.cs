015f9cdf15c5a96cf270bd46d1b0cff854589293
Jon Grythe Stødle <jon.stodle@webstep.no>
2020-12-03 09:01:51 +0100

    Add Luke03

---
+++ src/Luke03/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Luke03
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var matrix = File.ReadAllLines("matrix.txt").ToList();
+			Console.WriteLine(string.Join(",", 
+				File.ReadAllLines("wordlist.txt")
+				// words
+				.AsParallel()
+				.Where(word =>
+				{
+					var reverse = string.Join("", word.Reverse());
+
+					return
+						!(SearchHorizontal(matrix, word) || SearchHorizontal(matrix, reverse) ||
+						SearchVertical(matrix, word) || SearchVertical(matrix, reverse) ||
+						SearchRightDiagonal(matrix, word) || SearchRightDiagonal(matrix, reverse) ||
+						SearchLeftDiagonal(matrix, word) || SearchLeftDiagonal(matrix, reverse));
+				})
+				.OrderBy(word => word)));
+		}
+
+		private static bool SearchHorizontal(List<string> matrix, string word) => 
+			matrix.Any(line => line.Contains(word));
+
+		private static bool SearchVertical(List<string> matrix, string word)
+		{
+			for (int i = 0; i < matrix.Count - word.Length; i++)
+			{
+				for (int j = 0; j < matrix.Count; j++)
+				{
+					var hits = new bool[word.Length];
+					for (int k = 0; k < word.Length; k++)
+					{
+						if (word[k] == matrix[i + k][j])
+						{
+							hits[k] = true;
+						}
+					}
+
+					if (hits.All(h => h))
+						return true;
+				}
+			}
+
+			return false;
+		}
+
+		private static bool SearchRightDiagonal(List<string> matrix, string word)
+		{
+			for (int i = 0; i < matrix.Count - word.Length; i++)
+			{
+				for (int j = 0; j < matrix.Count - word.Length; j++)
+				{
+					var hits = new bool[word.Length];
+					for (int k = 0; k < word.Length; k++)
+					{
+						if (word[k] == matrix[i + k][j + k])
+						{
+							hits[k] = true;
+						}
+					}
+
+					if (hits.All(h => h))
+						return true;
+				}
+			}
+
+			return false;
+		}
+
+		private static bool SearchLeftDiagonal(List<string> matrix, string word)
+		{
+			for (int i = 0; i < matrix.Count - word.Length; i++)
+			{
+				for (int j = word.Length; j < matrix.Count; j++)
+				{
+					var hits = new bool[word.Length];
+					for (int k = 0; k < word.Length; k++)
+					{
+						if (word[k] == matrix[i + k][j - k])
+						{
+							hits[k] = true;
+						}
+					}
+
+					if (hits.All(h => h))
+						return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ src/fsharp/Luke03/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.IO
+
+let existsHorizontal (matrix: string list) (word: string) =
+    matrix
+    |> Seq.exists (fun line -> line.Contains word)
+    |> (fun exists -> not exists)
+    
+let existsHorizontalR (matrix: string list) (word: string) =
+    String.Join("", word.ToCharArray() |> Array.rev)
+    |> existsHorizontal matrix
+
+[<EntryPoint>]
+let main argv =
+    let matrix = File.ReadAllLines("matrix.txt") |> Array.toList
+    
+    File.ReadAllLines "wordlist.txt"
+    |> Seq.filter (existsHorizontal matrix)
+    |> (fun words -> String.Join(",", words))
+    |> Console.WriteLine
+    |> ignore
+    
+    0 // return an integer exit code
